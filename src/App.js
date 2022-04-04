aa1cd4be1ac3189348deddab7deda6936918fb73
khaizter <vashh.dev@gmail.com>
2022-04-05 06:57:40 +0800

    Added MoviePosterSection

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import MoviePosterList from "./components/MoviePosterList";
-import MoviePagination from "./components/MoviePagination";
+import React from "react";
+import MoviePosterSection from "./components/MoviePosterSection";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  useEffect(() => {
-    fetchMovies();
-  }, [currentPage]);
-
-  const fetchMovies = async () => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=eb56800a149ce9fa8f8b67af5e2a01ab&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${currentPage}&with_watch_monetization_types=flatrate`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-        console.log(data.results);
-      });
-  };
-
-  const nextPage = () => setCurrentPage((currentPage) => currentPage + 1);
-
-  const prevPage = () =>
-    setCurrentPage((currentValue) => {
-      if (currentValue === 1) return currentValue;
-      return currentValue - 1;
-    });
-
-  return (
-    <div className="App">
-      <h1>My Movie List</h1>
-      <MoviePosterList movies={movies} />
-      <MoviePagination nextPage={nextPage} prevPage={prevPage} />
-    </div>
-  );
+  return <MoviePosterSection />;
 }
 
 export default App;
