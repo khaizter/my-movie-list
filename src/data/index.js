bdc4066e5a97b124a69e8a3f5fdf90959cee0aab
khaizter <vashh.dev@gmail.com>
2022-04-09 14:36:19 +0800

    Added more information in detail page like cast etc.

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,10 @@
 const apiKey = "eb56800a149ce9fa8f8b67af5e2a01ab";
 const url = "https://api.themoviedb.org/3";
+export const imageUrl = "https://image.tmdb.org/t/p/";
 const discoverUrl = `${url}/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
 const nowPlayingUrl = `${url}/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
-const movieDetailsUrl = `https://api.themoviedb.org/3/movie/movieId?api_key=eb56800a149ce9fa8f8b67af5e2a01ab&language=en-US`;
+const movieDetailsUrl = `${url}/movie/movieId?api_key=${apiKey}&language=en-US`;
+const movieCreditsUrl = `${url}/movie/movieId/credits?api_key=${apiKey}&language=en-US`;
 
 export const fetchDiscover = async (page = 1) => {
   // fetch(discoverUrl.replace("page=1", `page=${page}`))
@@ -24,6 +26,12 @@ export const fetchNowPlaying = async () => {
 
 export const fetchMovieDetails = async (movieId) => {
   const response = await fetch(movieDetailsUrl.replace("movieId", movieId));
+  const data = await response.json();
+  return data;
+};
+
+export const fetchMovieCredits = async (movieId) => {
+  const response = await fetch(movieCreditsUrl.replace("movieId", movieId));
   const data = await response.json();
   return data;
 };
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,41 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
-import { fetchMovieDetails } from "../data";
+import { fetchMovieCredits, fetchMovieDetails, imageUrl } from "../data";
 import Error from "./Error";
 
 const MovieDetail = () => {
-  const [movieData, setMovieData] = useState({});
+  const [details, setDetails] = useState({});
+  const [credits, setCredits] = useState({});
   const { movieId } = useParams();
   const history = useHistory();
 
   useEffect(() => {
     const fetchApi = async () => {
-      const data = await fetchMovieDetails(movieId);
-      console.log(data);
-      setMovieData(data);
+      const detailsData = await fetchMovieDetails(movieId);
+      console.log(detailsData);
+      setDetails(detailsData);
+
+      const creditsData = await fetchMovieCredits(movieId);
+      console.log(creditsData);
+      setCredits(creditsData);
     };
     fetchApi();
   }, []);
 
-  if (!movieData) {
+  if (!details) {
     return <div>fetching data...</div>;
   }
 
   // if the api return a status_code instead it means the request fail and we render error instead
-  if (movieData.status_code) {
+  if (details.status_code) {
     return <Error />;
   }
 
-  if (movieData) {
+  if (details) {
     return (
       <div>
         Movie Details
-        <p>{movieData.title}</p>
-        <p>{movieData.overview}</p>
+        <img src={`${imageUrl}w342/${details.poster_path}`} alt=""></img>
+        <button>Trailer</button>
+        <p>{details.title}</p>
+        <p>{details.overview}</p>
+        <p>{details.runtime}</p>
+        <p>Genres</p>
         <ul>
-          {movieData.genres &&
-            movieData.genres.map((genre) => {
+          {details.genres &&
+            details.genres.map((genre) => {
               return <li key={genre.id}>{genre.name}</li>;
+            })}
+        </ul>
+        <p>Cast</p>
+        <ul>
+          {credits.cast &&
+            credits.cast.slice(0, 3).map((artist) => {
+              return (
+                <li key={artist.id}>
+                  <img
+                    src={`${imageUrl}w185/${artist.profile_path}`}
+                    alt=""
+                  ></img>
+                  {artist.name}
+                </li>
+              );
             })}
         </ul>
       </div>
