63fe771f8dac193078e3f05b65f582a6c6e74e9b
khaizter <vashh.dev@gmail.com>
2022-04-16 09:06:28 +0800

    Added search movie on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
 import classes from "./Header.module.css";
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useRef } from "react";
+import { Link, NavLink, useHistory } from "react-router-dom";
 
 const Header = () => {
+  const searchRef = useRef();
+  const history = useHistory();
+
+  const searchMovieHandler = () => {
+    const keywords = searchRef.current.value;
+    if (keywords.trim() !== "") {
+      history.push(`/movies?search_query=${keywords}&page=1`);
+    } else {
+      history.push("/movies?page=1");
+    }
+  };
+
   return (
     <header className={classes.header}>
       <Link to="/">Logo here!</Link>
       <ul className={classes.navigation}>
+        <li className={classes["search-movie"]}>
+          <input type="text" name="" id="" ref={searchRef} />
+          <button onClick={searchMovieHandler}>Search</button>
+        </li>
         <li>
           <NavLink to="/movies">Genres</NavLink>
         </li>
